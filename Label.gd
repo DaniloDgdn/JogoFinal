602f2c19ac514d74c244debb01ac2aaaff8fe5fc
Danilo <danilo.dgdn@gmail.com>
2019-06-04 10:57:31 -0300

    Adicionando o timer

---
+++ Label.gd
@@ -0,0 +1,15 @@
+extends Label
+
+var tempo = 10
+func _ready():
+	set_process(true)
+	
+	
+func _process(delta):
+	
+		tempo -= delta
+		if tempo > 0:
+			$".".set_text("Timer: "+str(tempo))
+		else:
+			get_tree().change_scene("GameOver.tscn")
+
